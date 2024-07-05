a95380daa60fb410c8ec0039ab2b62ee27b19c9f
sree-r-one <sreeraj_ec@hotmail.com>
2024-07-06 00:30:18 +0800

    pydantic models added

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,32 @@
 # region IMPORT
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
 
 # endregion IMPORT
 
 app = FastAPI()
 
 
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+
 @app.get("/")
 async def root():
     return {"message": "hello world again"}
+
+
+@app.get("/posts")
+async def get_posts():
+    return {"data": "This is your posts"}
+
+
+@app.post("/create")
+async def create_post(post: Post):
+    print(post)
+    return {"message": post}
